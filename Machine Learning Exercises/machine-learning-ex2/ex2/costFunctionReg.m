ea9323c35a7ef1aa5af26c72aa5ab5590039e63f
Ficci Tong <FicciTong@me.com>
2017-08-09 12:55:50 -0400

    Finished ex2, added ex3

--- Machine Learning Exercises/machine-learning-ex2/ex2/costFunctionReg.m
+++ Machine Learning Exercises/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,9 +17,10 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+[J, grad] = costFunction(theta, X, y);
 
-
-
+J = J + lambda / (2 * m) * sum( [0; theta(2:length(theta));] .^ 2 );
+grad = grad + (lambda / m) * [0; theta(2:length(theta));];
 
 
 % =============================================================
