c3ec1f55aecfa1870e528a3cf26a0f9c5f1da0d6
Ficci Tong <Ficci@Ficcis-MacBook-Pro.local>
2017-08-16 21:31:50 -0400

    Ex1 python progress

--- Machine Learning Exercises/machine-learning-ex1/ex1-python/ex1.py
+++ Machine Learning Exercises/machine-learning-ex1/ex1-python/ex1.py
@@ -2,4 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-print('hello')
+# Reading the data for ex1
+f = open('ex1data1.txt', mode='r')
+data = f.read()
+data = data.split('\n')
+data = data[:len(data) - 1]
+rows = []
+for i in data:
+    temp = i.split(',')
+    temp[0] = float(temp[0])
+    temp[1] = float(temp[1])
+    rows.append(temp)
